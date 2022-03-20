293f75044619b23981de985085da8be60462c146
srivathsanvenkateswaran <srivathsanvenkateswaran@gmail.com>
2022-03-20 16:44:57 +0530

    First and Last occurrence of an element

---
+++ 05_FirstAndLastOccurrenceOfAnElement.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstOccurrence(vector<int>& arr, int key)
+{
+    int left = 0;
+    int right = arr.size() - 1;
+
+    int mid = 0;
+
+    while(left <= right)
+    {
+        mid = left + (right - left)/2;
+
+        if(key == arr[mid])
+        {
+            if(mid && arr[mid-1] == key)
+            {
+                right = mid - 1;
+            }
+            else break;
+        }
+        else if(key > arr[mid])
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+
+    return mid;
+}
+
+int lastOccurrence(vector<int>& arr, int key)
+{
+    int left = 0;
+    int right = arr.size() - 1;
+
+    int mid = 0;
+
+    while(left <= right)
+    {
+        mid = left + (right - left)/2;
+
+        if(key == arr[mid])
+        {
+            if(mid && arr[mid+1] == key)
+            {
+                left = mid + 1;
+            }
+            else break;
+        }
+        else if(key > arr[mid])
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+
+    return mid;
+}
+
+int main()
+{
+    vector<int> arr = {2, 4, 10, 10, 10, 10, 18, 20};
+
+    int key = 10;
+
+    cout << firstOccurrence(arr, key) << '\n';
+    cout << lastOccurrence(arr, key) << '\n';
+
+    return 0;
+}
